8fe3023c046f37be4d37edb1b3c6641b0062ebe5
gavorilka <59140477+gavorilka@users.noreply.github.com>
2026-01-21 11:53:37 +0300

    Изменился ключ позиции

--- SITE_GET_POSITIONS_EMPLOYEES.sql
+++ SITE_GET_POSITIONS_EMPLOYEES.sql
@@ -5,21 +5,21 @@ RETURNS (
 AS
 BEGIN
 FOR
-    SELECT
-        DISTINCT V66.val
+     SELECT
+        DISTINCT V3543.val
     FROM
-        VALS V66
+        VALS V3543
     WHERE
-        V66.PARAM_ID = 66
-        AND V66.IS_DEL = 0
+        V3543.PARAM_ID = 3543
+        AND V3543.IS_DEL = 0
     ORDER BY
         CASE
-            WHEN LEFT(V66.val, 1) = 'У' THEN 0
-            WHEN LEFT(V66.val, 1) = 'П' THEN 1
-            WHEN LEFT(V66.val, 1) = 'З' THEN 3
-            WHEN LEFT(V66.val, 1) = 'М' THEN 4
-            WHEN LEFT(V66.val, 1) = 'С' THEN 5
-            WHEN LEFT(V66.val, 1) = 'Д' THEN 6
+            WHEN LEFT(V3543.val, 1) = 'У' THEN 0
+            WHEN LEFT(V3543.val, 1) = 'П' THEN 1
+            WHEN LEFT(V3543.val, 1) = 'З' THEN 3
+            WHEN LEFT(V3543.val, 1) = 'М' THEN 4
+            WHEN LEFT(V3543.val, 1) = 'С' THEN 5
+            WHEN LEFT(V3543.val, 1) = 'Д' THEN 6
             ELSE 10
         END
     INTO
